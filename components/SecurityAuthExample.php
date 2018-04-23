bc1f6b01abac39570e395c663d2a4af63c094907
seven <728443778@qq.com>
2018-04-23 09:34:08 +0300

    调整示例

--- components/SecurityAuthExample.php
+++ components/SecurityAuthExample.php
@@ -8,7 +8,7 @@ class SecurityAuthExample
         $params = [
             'auth_id' => $authId,
             'auth_time' => $time,
-            'auth_token' => hash('sha256', $authId . $time)
+            'auth_token' => hash('sha256', $authToken . $time)
         ];
         $return = \app\common\utils\HttpRequest::getInstance()->requestPost('http://dwadwa', $params);
         if (isset($return['code']) && $return['code'] == 0) {
