c6c177476c33831762107d15302f9e7650eb98f8
seven <728443778@qq.com>
2018-04-24 09:03:50 +0300

    add init

--- traits/SingleInstance.php
+++ traits/SingleInstance.php
@@ -15,6 +15,12 @@ trait SingleInstance
             return self::$_instance;
         }
         self::$_instance = new self();
+        self::$_instance->init(); //在构造函数中初始化出问题 有时不好找问题
         return self::$_instance;
+    }
+
+    public function init()
+    {
+
     }
 }
